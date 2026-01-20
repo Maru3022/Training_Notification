be984290c2662b540de0f4951de59bc6c7ae122a
Maru <pdijsj2992@gmail.com>
2026-01-20 21:59:35 +0300

    Initial commit

---
+++ src/main/java/com/example/training_notification/TrainingNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.example.training_notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingNotificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingNotificationApplication.class, args);
+    }
+
+}
