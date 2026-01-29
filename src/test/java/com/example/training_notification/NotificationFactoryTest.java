a88c51fc699bc4b0b05581c1f0e17fad21218c65
Maru <pdijsj2992@gmail.com>
2026-01-29 08:16:44 +0300

    Fixed a bug in NotificationFactoryTest

--- src/test/java/com/example/training_notification/NotificationFactoryTest.java
+++ src/test/java/com/example/training_notification/NotificationFactoryTest.java
@@ -7,22 +7,24 @@ import com.example.training_notification.service.interfaces.NotificationSender;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.util.ReflectionTestUtils;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class NotificationFactoryTest {
 
     @Mock
+    private NotificationSender emailSender;
+
+    @InjectMocks
     private NotificationFactory notificationFactory;
 
     @Test
@@ -30,19 +32,15 @@ public class NotificationFactoryTest {
     void getSender_EmailSuccess(){
         System.out.println("Testing Factory: searching for EMAIL sender");
 
-        NotificationSender mockEmailSender = mock(NotificationSender.class);
-        when(mockEmailSender.supports(NotificationType.EMAIL))
+        when(emailSender.supports(NotificationType.EMAIL))
                 .thenReturn(true);
 
-        List<NotificationSender> senders = new ArrayList<>();
-        senders.add(mockEmailSender);
-
-        ReflectionTestUtils.setField(notificationFactory, "senders", senders);
+        ReflectionTestUtils.setField(notificationFactory, "senders", List.of(emailSender));
         NotificationSender result = notificationFactory.getSender(NotificationType.EMAIL);
 
-        System.out.println("Factory returned: " + result.getClass().getSimpleName());
-        assertNotNull(result);
-        assertTrue(result.supports(NotificationType.EMAIL));
+        assertNotNull(result, "Resulting sender should not be null");
+        System.out.println("SUCCESS: Found sender " + result.getClass().getSimpleName());
+        assertEquals(emailSender,result);
 
     }
 }
