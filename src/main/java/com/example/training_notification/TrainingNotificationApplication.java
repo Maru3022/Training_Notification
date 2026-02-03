a742ff94382c6405615f2401184b21e7f1742cdc
Maru <pdijsj2992@gmail.com>
2026-02-03 15:46:07 +0300

    refactor: rename folders to lowercase for consistency

--- src/main/java/com/example/training_notification/TrainingNotificationApplication.java
+++ src/main/java/com/example/training_notification/TrainingNotificationApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-//ToDo: разобрать весь проект подробно,чтобы потом смог переписать самостоятельно
-
 @SpringBootApplication
 @Cacheable
 @EnableAsync
--- src/main/java/com/example/training_notification/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/training_notification/exception/GlobalExceptionHandler.java
@@ -1,4 +1,4 @@
-package com.example.training_notification.Exception;
+package com.example.training_notification.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
